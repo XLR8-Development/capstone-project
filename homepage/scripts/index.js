a33576bc25e4d2369ef153ed6cbc059ae7228157
jfed8 <jadenfeddock@gmail.com>
2017-12-12 14:38:45 -0700

    Character count

--- homepage/scripts/index.js
+++ homepage/scripts/index.js
@@ -1,6 +1,22 @@
 (function(context) {
-    
+
     // utc_epoch comes from index.py
     console.log('Current epoch in UTC is ' + context.utc_epoch);
-    
+
 })(DMP_CONTEXT.get());
+
+$(function() {
+
+})
+
+// Function for counting characters
+$("#id_tweet").on('input', function(){
+    var currentLength = this.value.length;
+
+    $('#charNum').text((140 - currentLength) + " characters remaining");
+    if (currentLength < 50) {
+      $('#charNum').css('color', '#000000');
+    } else {
+      $('#charNum').css('color', '#ff0000');
+    }
+});
